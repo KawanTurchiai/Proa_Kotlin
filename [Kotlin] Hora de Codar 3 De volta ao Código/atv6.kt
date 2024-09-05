b8ec64c7ce150dd2f5d687a27656be863ccb3ac5
KawanTurchiai <kawanbarbosa150@gmail.com>
2024-09-05 10:01:11 -0300

    kotlin 3 e 4

---
+++ [Kotlin] Hora de Codar 3 De volta ao Código/atv6.kt
@@ -0,0 +1,44 @@
+val listAluno = mutableListOf<String>()
+
+fun main(){
+    println("Seja Bem Vindo Minha Criança")
+    alunos()
+    adicionar()
+}
+
+fun alunos(){
+    print("Insira seu nome Aluno: ")
+    var nome_aluno = readln()
+
+    print("Insira a primeira nota: ")
+    var nota1 = readln().toDouble()
+
+
+    print("Insira a segunda nota: ")
+    var nota2 = readln().toDouble()
+
+    var media: Double
+    media = (nota1 + nota2) / 2
+
+    if(media >= 9.5){
+        println("Você foi Aprovado!")
+        listAluno.add(nome_aluno)
+    }else{
+        println("Você foi Reprovado!")
+    }
+}
+
+ fun adicionar(){
+     println("Calcular a média de outro aluno Sim/Não?")
+     var opcao = readln().lowercase()
+
+     while(opcao == "s"){
+         alunos()
+     }
+
+     if(opcao == "n"){
+         println("Valeu!")
+     }
+
+     println("Foram aprovados "+ listAluno.count() +" alunos" + "\nNome das Crianças: " + listAluno)
+ }
