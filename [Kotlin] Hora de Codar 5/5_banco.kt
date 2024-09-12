d9531055e8134315b9b37df558b0d7a15276bbdb
KawanTurchiai <kawanbarbosa150@gmail.com>
2024-09-11 21:37:04 -0300

    correção de bugs
    Inclusão da validação de cada função do software

--- [Kotlin] Hora de Codar 5/5_banco.kt
+++ [Kotlin] Hora de Codar 5/5_banco.kt
@@ -1,62 +1,83 @@
 var saldo = 150.0
 fun main() {
-    println("Qual o seu nome?")
-    val nome = readln()
+    println("Qual o seu nome? ")
+    var nome = readln().toString()
     println("Olá. $nome")
     login(nome)
 }
-
 fun login(nome: String) {
-    println("$nome, nos informe qual a senha?")
-    val senha = readln().toInt()
-    if (senha == 3589) {
-        opcoes();
+
+    if (vali(nome)) {
+        opcoes(nome)
     } else {
-        println("Erro!\nTente novamente")
+        error()
+        login(nome)
     }
 }
-
-fun opcoes() {
+fun vali(nome: String): Boolean {
+    println("$nome, nos informe qual a senha?")
+    val senha = readln().toInt()
+    return senha == 3589
+}
+fun opcoes(nome: String) {
     println(
-        "Selecione uma opção\n1. Saldo \n2.Extrato \n3. Saque \n4. Deposito\n5. Transferir\n6. Sair"
+        "Selecione uma opção\n1. Saldo \n2 .Extrato \n3. Saque \n4. Deposito\n5. Transferir\n6. Sair"
     )
     var opcao = readln().toInt()
     when {
-        opcao == 1 -> { sal() }
-        opcao == 2 -> { ext() }
-        opcao == 3 -> { saq() }
-        opcao == 4 -> { dep()}
-        opcao == 5 -> { trans()}
-        opcao == 6 -> { exit()}
+        opcao == 1 -> if (vali(nome)){sal(nome)} else  error()
+        opcao == 2 -> if (vali(nome)){ext(nome)} else  error()
+        opcao == 3 -> if (vali(nome)){saq(nome)} else  error()
+        opcao == 4 -> if (vali(nome)){dep(nome)} else  error()
+        opcao == 5 -> if (vali(nome)){transfer(nome)} else  error()
+        opcao == 6 -> if (vali(nome)){exit(nome)} else  error()
     }
 }
-
-fun sal() {
+fun sal(nome: String) {
     println("O seu saldo é de R$$saldo")
-    opcoes()
+    opcoes(nome)
 }
-
-fun ext() {
-    println("Quanto deseja sacar?")
+fun ext(nome: String) {
+    print("Extrato referente ao mês de Setembro")
+    println("- 87,00 Outback")
+    println("- 47,00 Renner")
+    println("- 91,00 Pizzaria Pizzaboa")
+    println("+ 1.412,00 Salário")
+    opcoes(nome)
 }
-fun saq() {
-
+fun saq(nome: String) {
     println("Quanto deseja sacar?")
     var saque = readln().toDouble()
-    if (saldo >= saque) {
-        saldo = saldo - saque
-        println("Saque efetuado!\nSaldo = R$$saldo")
+    if (saldo < saque || saque <= 0) {
+        println("Operação não autorizada")
+        saq(nome);
+
     } else {
-        println("Vai trabalhar, seu pobre! faz o L")
-        saq();
+        saldo -= saque
+        println("Saque efetuado!\nSaldo = R$$saldo")
     }
-    opcoes();
+    opcoes(nome);
 }
+fun dep(nome: String) {
 
-fun dep() {
     println("Quanto deseja depositar?")
+    var deposito = readln().toDouble()
+    saldo += deposito
+    println("Deposito efetuado!\nSaldo = R$$saldo")
+    opcoes(nome);
 }
-
-fun exit() {
-    println("$Nome, foi um prazer ter você por aqui!")
+fun transfer(nome: String) {
+    println("Para quem deseja tranferir?")
+    var pessoa = readln()
+    println("Quanto deseja tranferir?")
+    var value = readln().toDouble()
+    saldo -= value
+    println("A tranferencia de R$$value foi enviado para $pessoa, Saldo = R\$$saldo")
+    opcoes(nome)
+}
+fun exit(nome: String) {
+    println("$nome, foi um prazer ter você por aqui!")
+}
+fun error (){
+    println("ERRO!")
 }
